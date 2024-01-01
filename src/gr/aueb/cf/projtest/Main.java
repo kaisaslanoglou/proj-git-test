a82e57399652c3fa7fa57064aca346a84dd7c826
K. Aslanoglou <k_aslanoglou@hotmail.com>
2024-01-01 18:24:21 +0200

    Main updated II

--- src/gr/aueb/cf/projtest/Main.java
+++ src/gr/aueb/cf/projtest/Main.java
@@ -2,6 +2,6 @@ package gr.aueb.cf.projtest;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test Git commands using dev branch");
+        System.out.println("Test Git commands using dev branch and again!");
     }
 }
