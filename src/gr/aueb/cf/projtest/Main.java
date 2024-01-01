52620cbe7c3d54d6d621cedbcb4529481cfcc08a
K. Aslanoglou <k_aslanoglou@hotmail.com>
2024-01-01 18:36:57 +0200

    sayHello Method

--- src/gr/aueb/cf/projtest/Main.java
+++ src/gr/aueb/cf/projtest/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Test Git commands using dev branch and again!");
     }
+
+    public static void sayHello() {
+        System.out.println("Hello feature");
+    }
 }
